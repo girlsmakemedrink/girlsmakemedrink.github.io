b8a1547497fffd51c92db261fe68461391ea3247
girlsmakemedrink <97946318+girlsmakemedrink@users.noreply.github.com>
2023-06-24 14:00:24 +0700

    Delete cv_git-main directory

--- cv_git-main/js.js
+++
@@ -1,9 +0,0 @@
-/* Это объявление переменной, мы наши кнопку по тегу */
-const audio = new Audio("./sound.mp3");
-const buttons = document.querySelectorAll(".kauvec_adelon");
-
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-  audio.play();
-  });
-});
